fe5f62c3b73dc9ca889f251a2f0f883e9cff46e9
Clinton Selke <clinuxrulz@gmail.com>
2023-09-20 11:52:09 +1000

    wip

--- src/camera.rs
+++ src/camera.rs
@@ -1,8 +1,18 @@
-use crate::Transform3;
+use crate::{Ray2, Transform3, Vec2, Vec3};
 
 pub struct Camera<T> {
     pub space: Transform3<T>,
     pub screen_width: T,
     pub screen_height: T,
     pub screen_dist: T,
+}
+
+impl<T> Camera<T> {
+    pub fn screen_x_to_ray2(x: T) -> Option<Ray2<T>> {
+        todo!();
+    }
+
+    pub fn project(pt: Vec3<T>) -> Vec2<T> {
+        todo!();
+    }
 }
