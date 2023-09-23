6d534929cb47e178d9953def2459253b5eb7d997
Clinton Selke <clinuxrulz@gmail.com>
2023-09-23 23:49:03 +1000

    more methods for transform3

--- src/transform3.rs
+++ src/transform3.rs
@@ -1,15 +1,45 @@
-use crate::{Quaternion, Vec3};
+use std::ops::{Add, Sub, Mul, Neg};
+
+use crate::{Quaternion, Vec3, Zero};
 
 pub struct Transform3<T> {
     pub origin: Vec3<T>,
     pub orientation: Quaternion<T>,
 }
 
-impl<T> Transform3<T> {
+impl<T: Copy> Transform3<T> {
     pub fn new(origin: Vec3<T>, orientation: Quaternion<T>) -> Transform3<T> {
         Transform3 {
             origin,
             orientation,
         }
+    }
+
+    pub fn point_from_space(&self, pt: Vec3<T>) -> Vec3<T>
+    where
+        T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Neg<Output=T> + Zero
+    {
+        self.vector_from_space(pt) + self.origin
+    }
+
+    pub fn point_to_space(&self, pt: Vec3<T>) -> Vec3<T>
+    where
+        T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Neg<Output=T> + Zero
+    {
+        self.vector_to_space(pt - self.origin)
+    }
+
+    pub fn vector_from_space(&self, v: Vec3<T>) -> Vec3<T>
+    where
+        T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Neg<Output=T> + Zero
+    {
+        self.orientation.rotate(v)
+    }
+
+    pub fn vector_to_space(&self, v: Vec3<T>) -> Vec3<T>
+    where
+        T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Neg<Output=T> + Zero
+    {
+        self.orientation.conjugate().rotate(v)
     }
 }
