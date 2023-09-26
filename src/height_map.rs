3a0986d3bda9993f8535eeca2609f7b4d8486234
Clinton Selke <clinuxrulz@gmail.com>
2023-09-27 01:06:30 +1000

    wip

--- src/height_map.rs
+++ src/height_map.rs
@@ -1,6 +1,6 @@
 use rand::prelude::random;
 
-use crate::QuadTree;
+use crate::{QuadTree, Ray2, Vec2};
 
 pub struct HeightMap {
     num_levels: usize,
@@ -47,7 +47,55 @@ impl HeightMap {
         self.quad_tree.set_value(level, x, y, val);
     }
 
-    pub fn read(&mut self, level: usize, x: usize, y: usize) -> f64 {
+    pub fn read(&self, level: usize, x: usize, y: usize) -> f64 {
         *self.quad_tree.get_value(level, x, y)
+    }
+
+    pub fn ray_xz_insection_2pt5d(&self, ray_xz: Ray2<f64>) -> Option<(f64,f64,f64)> {
+        self.ray_xz_insection_2pt5d_2(0, 0, 0, ray_xz)
+    }
+
+    fn ray_xz_insection_2pt5d_2(&self, depth: usize, x0: usize, y0: usize, ray_xz: Ray2<f64>) -> Option<(f64,f64,f64)> {
+        let size: usize = 1 << (self.num_levels-1-depth);
+        let size2 = size as f64;
+        let t1 = (-0.5 * size2 - ray_xz.origin.x) / ray_xz.direction.x;
+        let t2 = (0.5 * size2 - ray_xz.origin.x) / ray_xz.direction.x;
+        let t3 = (-0.5 * size2 - ray_xz.origin.y) / ray_xz.direction.y;
+        let t4 = (0.5 * size2 - ray_xz.origin.y) / ray_xz.direction.y;
+        let t_min = t1.min(t2).max(t3.min(t4));
+        let t_max = t1.max(t2).min(t3.max(t4));
+        if t_min <= 0.0 {
+            return None;
+        }
+        if t_max < t_min {
+            return None;
+        }
+        if depth < self.num_levels-1 {
+            let half_size = size >> 1;
+            let offsets: [([f64; 2],[usize; 2]); 4] = [
+                ([-0.5 * size2, -0.5 * size2], [0, 0]),
+                ([0.5 * size2, -0.5 * size2], [half_size, 0]),
+                ([0.5 * size2, 0.5 * size2], [half_size, half_size]),
+                ([-0.5 * size2, 0.5 * size2], [0, half_size]),
+            ];
+            let mut closest: Option<(f64,f64,f64)> = None;
+            for (ray_offset, index_offset) in offsets {
+                let ray_xz2 = Ray2::new(ray_xz.origin + Vec2::new(ray_offset[0], ray_offset[1]), ray_xz.direction);
+                let r = self.ray_xz_insection_2pt5d_2(depth + 1, index_offset[0], index_offset[1], ray_xz2);
+                if let Some(r2) = r {
+                    if closest.is_none() || r2.0 < closest.unwrap().0 {
+                        closest = Some(r2);
+                    }
+                }
+            }
+            closest
+        } else {
+            let t = 0.5 * (t_min + t_max);
+            let pt = ray_xz.position_from_time(t);
+            let x = x0 + (((pt.x + 0.5 * size2) as i32).max(0).min((size-1) as i32) as usize);
+            let y = y0 + (((pt.y + 0.5 * size2) as i32).max(0).min((size-1) as i32) as usize);
+            let height = self.read(depth, x, y);
+            Some((t_min,t_max,height))
+        }
     }
 }
